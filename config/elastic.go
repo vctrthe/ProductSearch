b56c79f175e765cec48160a25ccb082823622f4c
Victor The <victorthe123@gmail.com>
2025-04-15 23:57:07 +0700

    [ProductSearch] added debugger for Elasticsearch connection

--- config/elastic.go
+++ config/elastic.go
@@ -11,12 +11,20 @@ var ES *elasticsearch.Client
 func InitElastic() {
 	cfg := elasticsearch.Config{
 		Addresses: []string{"http://192.168.100.22:9200"},
+		Username:  "elastic",
+		Password:  "password",
 	}
 
-	es, err := elasticsearch.NewClient(cfg)
+	client, err := elasticsearch.NewClient(cfg)
 	if err != nil {
 		log.Fatalf("elastic init error: %s", err)
 	}
 
-	ES = es
+	_, err = client.Info()
+	if err != nil {
+		log.Fatalf("elasticsearch connection failed: %s", err)
+	}
+
+	log.Println("elasticsearch connection established")
+	ES = client
 }
