7e0415e4e65f8bdade9d7a96a456215898d15fe1
Victor The <victorthe123@gmail.com>
2025-04-15 23:53:27 +0700

    [ProductSearch] Fix Missing ElasticSearch Init Command and CSV loader function

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/vctrthe/ProductSearch/config"
 	"github.com/vctrthe/ProductSearch/controller"
+	"github.com/vctrthe/ProductSearch/util"
 )
 
 func main() {
+	config.InitElastic()
+	util.LoadAndIndexData(config.ES)
+
 	router := gin.Default()
 	router.GET("/search", controller.SearchProduct)
 	router.Run(":8080")
